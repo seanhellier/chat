75c60eed4def4a469c411130baa385d30b1a99bf
Sean Leif Hellier <sean@newtropolis.com>
2020-05-23 11:08:51 -0400

    finished the server socket functions

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -9,6 +9,9 @@ let socket;
 const Chat = ({ location }) => {
 	const [name, setName] = useState("");
 	const [room, setRoom] = useState("");
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+
 	const ENDPOINT = "localhost:5000";
 
 	useEffect(() => {
@@ -28,7 +31,30 @@ const Chat = ({ location }) => {
 		};
 	}, [ENDPOINT, location.search]);
 
-	return <h1>Chat</h1>;
+	useEffect(() => {
+		socket.on("message", (message) => {});
+		setMessages([...messages, message]);
+	}, [messages]);
+
+	const sendMessage = (event) => {
+		if (message) {
+			socket.emit("sendMessage", message, () => setMessage(""));
+		}
+	};
+
+	return (
+		<div className="outerContainer">
+			<div className="container">
+				<input
+					value={message}
+					onChange={(event) => setMessage(event.target.value)}
+					onKeyPress={(event) =>
+						event.key === "Enter" ? sendMessage(event) : null
+					}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Chat;
